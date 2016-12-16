cc2eef42bc8e1e78aaaf784a0e096bd8a9db5a30
Guure <543449492@qq.com>
2016-12-16 14:02:41 +0800

    how

--- First Line of Code/5.3 Send Standard-custom Broadcast.java
+++ First Line of Code/5.3 Send Standard-custom Broadcast.java
@@ -3,12 +3,15 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
+        // 如果接收有序广播，可以使用abortBroadcast()截断广播
+        // abortBroadcast();
     }
 }
 
 /*
 
 	静态注册广播接收器
+	可以使用<intent-filter android:priority="100">规定广播接收器的优先级，默认为0（参考API文档）
   <receiver android:name=".MyBroadcastReceiver">
       <intent-filter>
       		自定义广播
@@ -31,5 +34,7 @@ button.setOnClickListener(new View.OnClickListener() {
     		// 发送自定义广播
         Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
         sendBroadcast(intent);
+        // 发送有序广播
+        // sendOrderedBroadcast(intent, null);
     }
 });
