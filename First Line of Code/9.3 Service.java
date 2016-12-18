1db886454f3d44121a3127d9a6fb989d10c84787
Guure <543449492@qq.com>
2016-12-18 23:59:25 +0800

    Service

---
+++ First Line of Code/9.3 Service.java
@@ -0,0 +1,111 @@
+/* MyService.java */
+
+public class MyService extends Service {
+
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+
+        public void startDownload() {
+            Log.d("MyService", "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.d("MyService", "getProgress executed");
+            return 0;
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy executed");
+    }
+
+}
+
+/* MainActivity.java */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button startService;
+
+    private Button stopService;
+
+    private Button bindService;
+
+    private Button unbindService;
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder) iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startService = (Button) findViewById(R.id.start_service);
+        stopService = (Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+
+        bindService = (Button) findViewById(R.id.bind_service);
+        unbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent); // 启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent); // 停止服务
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE); // 绑定服务
+                break;
+            case R.id.unbind_service:
+                unbindService(connection); // 解绑服务
+                break;
+            default:
+                break;
+        }
+    }
+}
