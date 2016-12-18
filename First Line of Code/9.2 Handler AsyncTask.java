78f2c8689056792b5625d74a8adedea1e7331cc9
Guure <543449492@qq.com>
2016-12-18 21:13:54 +0800

    Async

---
+++ First Line of Code/9.2 Handler AsyncTask.java
@@ -0,0 +1,50 @@
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int UPDATE_TEXT = 1;
+
+    private TextView text;
+
+    private Button changeText;
+
+    private Handler handler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    // 在这里可以进行UI操作
+                    text.setText("Nice to meet you");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+        changeText = (Button) findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message); // 将Message对象发送出去
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+    }
+}
