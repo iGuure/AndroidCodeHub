a32459c1df7836065c92301680d479ad6aa4ecd0
hsb <hsb@hsbs-iMac.local>
2017-07-18 18:12:39 +0800

    add test to special fonts

--- CustomView/CustomLetterSpacing/app/src/main/java/com/example/hsb/customletterspacing/MainActivity.java
+++ CustomView/CustomLetterSpacing/app/src/main/java/com/example/hsb/customletterspacing/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hsb.customletterspacing;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // test special fonts
+        TextView textView = (TextView) findViewById(R.id.specialFont);
+        Typeface font = Typeface.createFromAsset(getAssets(), "credit_limit_font.ttf");
+        textView.setTypeface(font);
     }
 }
